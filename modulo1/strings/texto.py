640ef378ab96e91a2715106d3afa40a81f1087de
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2024-05-24 16:45:06 -0300

    work with string

--- modulo1/strings/texto.py
+++ modulo1/strings/texto.py
@@ -1,3 +1,14 @@
 nome = 'Fabio'
 nome_completo = "Fabio Nogueira"
 
+# Retorna a string toda em maiscula
+#print(nome.upper())
+
+# Retorna a string toda em minuscula
+#print(nome.lower())
+
+# Retorna uma posicao da string 
+print(nome[0])
+
+print(nome)
+print(nome_completo)
