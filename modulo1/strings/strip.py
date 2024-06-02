9d4e356c4bd34dbfc1082ac42a05d3a85ad1f052
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-06-02 20:33:10 -0300

    work with strings

---
+++ modulo1/strings/strip.py
@@ -0,0 +1,14 @@
+nome_completo = 'xFabio Nogueirax'
+
+""" print(nome_completo.strip('x'))
+
+print(nome_completo.strip('a'))
+
+print(nome_completo.rstrip('x'))
+
+print(nome_completo.startswith('Fa'))
+
+print(nome_completo.startswith('xFa')) """
+
+print("abc" in nome_completo)
+
