e396fd702e7544038b333752a2af97562319ee90
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-06-05 21:26:32 -0300

    Continue list

--- modulo1/listas/lista1.py
+++ modulo1/listas/lista1.py
@@ -9,3 +9,11 @@ print("Minha lista", my_list)
 my_list.append(2)
 
 print("Minha lista", my_list)
+
+indice = my_list.index(1)
+
+print("O indice é do elemento 1",indice)
+
+my_list.insert(2,10)
+
+print("Minha lista 2,10", my_list)
