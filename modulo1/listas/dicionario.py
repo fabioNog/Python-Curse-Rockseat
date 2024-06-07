9eeb842e6cafbf8934ffab4eabec1cc639d9787e
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-06-06 21:29:41 -0300

    Metodos

--- modulo1/listas/dicionario.py
+++ modulo1/listas/dicionario.py
@@ -1,3 +1,27 @@
 my_dictionary = {"name": "Fabio","idade": 20, "cidade": "SaoPaulo"}
 
 print(my_dictionary["name"])
+
+
+del my_dictionary["cidade"]
+
+print(my_dictionary)
+
+my_dictionary["skill"] = "javascript"
+
+print(my_dictionary)
+
+
+chaves = my_dictionary.keys()
+
+print(chaves)
+
+
+keys = list(my_dictionary.keys())
+
+print(keys)
+
+
+values = my_dictionary.values()
+
+print(values)
