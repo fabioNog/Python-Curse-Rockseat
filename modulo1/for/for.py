a885f79ffc28e7a8ff3d9ec607733849c7313249
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2024-06-08 15:20:06 -0300

    For em listas tuplas e dicionario

---
+++ modulo1/for/for.py
@@ -0,0 +1,20 @@
+lista = [10,20,30]
+
+print("For utilizando lista "),
+for l in lista:
+    print(l)
+
+
+tupla = (10,20,30)
+
+print("For utilizando tupla "),
+for t in tupla:
+    print(t)
+    
+dicionario = {"nome": "Fabio","Idade": 10}
+
+print("For utilizando dicionario "),
+for chave,valor in dicionario.items():
+    print(f"{chave}: {valor}")
+    
+
